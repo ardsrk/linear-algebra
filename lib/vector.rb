60f6392bf7da2ca415d3bc1674c239f51f679110
Arvind L <arvind.laxminarayan@moviuscorp.com>
2021-07-04 14:02:46 +0530

    dot product of vectors

--- lib/vector.rb
+++ lib/vector.rb
@@ -33,11 +33,30 @@ class Vector
       el * num
     end)
   end
+
+  # Dot product, Inner scalar, or projection product
+  def dot(av)
+    if self.count != av.count
+      raise "Length of vectors are not the same."
+    end
+
+    self.zip(av).collect do |a, b|
+      a * b
+    end.sum
+  end
+
+  def size
+    Math.sqrt(dot(self))
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
-  v = Vector.new([1,2,3,4])
-  puts "v     = #{v}"
-  puts "v + v = #{v + v}"
-  puts "v * 2 = #{v * 2}"
+  r = Vector.new([3,4])
+  s = Vector.new([5,6])
+  puts "r       = #{r}"
+  puts "r + r   = #{r + r}"
+  puts "r * 2   = #{r * 2}"
+  puts "s       = #{s}"
+  puts "r . s   = #{r.dot(s)}"
+  puts "size(r) = #{r.size}"
 end
