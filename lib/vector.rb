96cc06cb7685fc782b3196656a0e16aa822b8f8f
Arvind L <ardsrk@gmail.com>
2021-07-05 16:07:18 +0530

    add Nearli namespace

--- lib/vector.rb
+++ lib/vector.rb
@@ -1,85 +1,87 @@
-class Vector
+module Nearli
+  class Vector
 
-  include Enumerable
+    include Enumerable
 
-  def initialize(vector)
-    @vector = vector
-  end
-
-  def each(&block)
-    if block_given?
-      @vector.each { |el| yield(el) }
-    else
-      @vector.each
+    def initialize(vector)
+      @vector = vector
     end
-  end
 
-  def to_s
-    "[#{collect.to_a.join(', ')}]"
-  end
+    def each(&block)
+      if block_given?
+        @vector.each { |el| yield(el) }
+      else
+        @vector.each
+      end
+    end
 
-  def +(av)
-    if self.count == av.count
-      Vector.new(self.zip(av).collect do |a, b|
-        a + b
+    def to_s
+      "[#{collect.to_a.join(', ')}]"
+    end
+
+    def +(av)
+      if self.count == av.count
+        Vector.new(self.zip(av).collect do |a, b|
+          a + b
+        end)
+      else
+        raise "Length of vectors are not the same."
+      end
+    end
+
+    def *(num)
+      Vector.new(collect do |el|
+        el * num
       end)
-    else
-      raise "Length of vectors are not the same."
-    end
-  end
-
-  def *(num)
-    Vector.new(collect do |el|
-      el * num
-    end)
-  end
-
-  # Dot product, Inner scalar, or projection product
-  # See Vector#angle
-  def dot(av)
-    if self.count != av.count
-      raise "Length of vectors are not the same."
     end
 
-    self.zip(av).collect do |a, b|
-      a * b
-    end.sum
-  end
+    # Dot product, Inner scalar, or projection product
+    # See Vector#angle
+    def dot(av)
+      if self.count != av.count
+        raise "Length of vectors are not the same."
+      end
 
-  def size
-    Math.sqrt(dot(self))
-  end
+      self.zip(av).collect do |a, b|
+        a * b
+      end.sum
+    end
 
-  # Angle between vectors in degrees.
-  # Cosine rule: r . s = |r||s|cos(theta)
-  # The cosine rule implies that dot product of two
-  # orthogonal vectors is zero.
-  def angle(av)
-    cos_t = self.dot(av)/(size * av.size)
-    angle_in_radians = Math.acos(cos_t)
-    (180/Math::PI) * angle_in_radians
-  end
+    def size
+      Math.sqrt(dot(self))
+    end
 
-  def scalar_projection_on(av)
-    av.dot(self) / av.size.to_f
-  end
+    # Angle between vectors in degrees.
+    # Cosine rule: r . s = |r||s|cos(theta)
+    # The cosine rule implies that dot product of two
+    # orthogonal vectors is zero.
+    def angle(av)
+      cos_t = self.dot(av)/(size * av.size)
+      angle_in_radians = Math.acos(cos_t)
+      (180/Math::PI) * angle_in_radians
+    end
 
-  def vector_projection_on(av)
-    av * (scalar_projection_on(av) / av.size)
-  end
+    def scalar_projection_on(av)
+      av.dot(self) / av.size.to_f
+    end
 
-  # Change of basis from [1,0] and [0,1] to v1 and v2
-  def to_basis(v1, v2)
-    projection_on_v1 = self.vector_projection_on(v1)
-    projection_on_v2 = self.vector_projection_on(v2)
+    def vector_projection_on(av)
+      av * (scalar_projection_on(av) / av.size)
+    end
 
-    Vector.new([projection_on_v1.size / v1.size, projection_on_v2.size / v2.size])
+    # Change of basis from [1,0] and [0,1] to v1 and v2
+    def to_basis(v1, v2)
+      projection_on_v1 = self.vector_projection_on(v1)
+      projection_on_v2 = self.vector_projection_on(v2)
+
+      Vector.new([projection_on_v1.size / v1.size, projection_on_v2.size / v2.size])
+    end
   end
 end
 
 if $PROGRAM_NAME == __FILE__
-  r = Vector.new([1,0])
-  s = Vector.new([0,1])
+  r = Nearli::Vector.new([1,0])
+  s = Nearli::Vector.new([0,1])
   puts "r          = #{r}"
   puts "s          = #{s}"
   puts "r + r      = #{r + r}"
@@ -89,8 +91,8 @@ if $PROGRAM_NAME == __FILE__
   puts "r.angle(s) = #{r.angle(s)}"
 
   puts "\n\n==== Projection ====\n\n"
-  r = Vector.new([2,2])
-  s = Vector.new([1,1])
+  r = Nearli::Vector.new([2,2])
+  s = Nearli::Vector.new([1,1])
   puts "r = #{r}"
   puts "s = #{s}"
   puts "Scalar projection of s on r = #{s.scalar_projection_on(r)}"
@@ -99,9 +101,9 @@ if $PROGRAM_NAME == __FILE__
 
   puts "\n\n==== Change of Basis ====\n\n"
 
-  v1 = Vector.new([2,1])
-  v2 = Vector.new([-2,4])
-  r = Vector.new([3,4])
+  v1 = Nearli::Vector.new([2,1])
+  v2 = Nearli::Vector.new([-2,4])
+  r = Nearli::Vector.new([3,4])
   puts "r  = #{r}"
   puts "v1 = #{v1}"
   puts "v2 = #{v2}"
