3ac27c51a86055b040b179e8b3ce493a3af09521
Arvind L <ardsrk@gmail.com>
2021-07-08 20:01:37 +0530

    Add RowVector#dup and RowVector#collect!

--- lib/row_vector.rb
+++ lib/row_vector.rb
@@ -7,6 +7,20 @@ module Nearli
       @vector = vector
     end
 
+    def dup
+      RowVector.new(@vector.dup)
+    end
+
+    def collect!(&block)
+      if block_given?
+        @vector.each_with_index do |el, i|
+          @vector[i] = yield(el)
+        end
+      else
+        @vector.to_enum(:collect!)
+      end
+    end
+
     def each(&block)
       if block_given?
         @vector.each { |el| yield(el) }
